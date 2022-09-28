36eff0b6b6e2a49488a1c6aa00ed594b40694bcd
Erval Souza <ervalsouza20@gmail.com>
2022-09-27 21:12:10 -0300

    Finalizacao das Mascaras

--- src/app/pages/novo-form/novo-form.component.ts
+++ src/app/pages/novo-form/novo-form.component.ts
@@ -2,6 +2,8 @@ import { CarroService } from './../../services/carro.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Carro } from 'src/app/Carro';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-novo-form',
@@ -14,14 +16,14 @@ export class NovoFormComponent implements OnInit {
 
 carroForm!: FormGroup
 
-  constructor(private carroService:CarroService) { }
+  constructor(private carroService:CarroService,private router:Router) { }
 
   ngOnInit(): void {
     this.carroForm = new FormGroup({
       id: new FormControl(''),
       marca_carro: new FormControl(''),
       dc_modelo: new FormControl(''),
-      dc_ano_fabricacao: new FormControl('', [Validators.maxLength(4)]),
+      dc_ano_fabricacao: new FormControl('',[Validators.required, Validators.maxLength(4), Validators.minLength(4)] ),
       dt_compra: new FormControl(''),
       nm_valor: new FormControl('', [Validators.required]),
     })
@@ -47,6 +49,9 @@ get marca_carro(){
   return this.carroForm.get('marca_carro')!
 }
 
+
+
+
 submit(){
   if(this.carroForm.invalid){
     return;
@@ -54,7 +59,10 @@ submit(){
 
 this.onSubmit.emit(this.carroForm.value)
 
+
   console.log(this.carroForm.value)
+
+  this.router.navigateByUrl("")
 
 
 }
